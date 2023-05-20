6e4fefe970eb15ef6c2822a7dba03e6632930db6
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-05-19 21:35:11 -0400

    improve login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,8 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen">
-      <div className="mx-auto my-auto w-9/12 max-w-screen-xl rounded-md px-4 py-16 shadow drop-shadow-sm sm:px-6 md:w-6/12 lg:w-4/12 lg:px-8">
+    <div className="flex min-h-screen bg-gradient-to-b from-cyan-300 via-sky-500 to-blue-800">
+      <div className="mx-auto my-auto w-9/12 max-w-screen-xl rounded-xl bg-white px-4 py-16 shadow drop-shadow-xl sm:px-6 md:w-6/12 lg:w-4/12 lg:px-8">
         <div className="mx-auto max-w-lg text-center">
           <h1 className="text-2xl font-bold text-black sm:text-3xl">
             🔵 huddle
@@ -16,7 +16,7 @@ export default function Login() {
           <button
             aria-label="Continue with google"
             role="button"
-            className="mt-10 flex items-center rounded-lg border border-gray-900 px-4 py-3.5 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-1"
+            className="mt-10 flex items-center rounded-lg border border-gray-900 px-7 py-3.5 transition hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-1"
             onClick={() => signIn("google")}
           >
             <svg
@@ -43,14 +43,16 @@ export default function Login() {
                 fill="#EB4335"
               />
             </svg>
-            <p className="ml-4 text-sm text-gray-900">Continue with Google</p>
+            <p className="ml-4 text-sm font-medium text-gray-900">
+              Continue with Google
+            </p>
           </button>
         </div>
         <div className="flex items-center justify-center">
           <button
             aria-label="Continue with google"
             role="button"
-            className="mt-3 flex items-center rounded-lg border border-gray-900 px-4 py-3.5 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-1"
+            className="mt-3 flex items-center rounded-lg border border-gray-900 px-7 py-3.5 transition hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-1"
             onClick={() => signIn("github")}
           >
             <svg
@@ -61,7 +63,9 @@ export default function Login() {
             >
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
             </svg>
-            <p className="ml-4 text-sm text-gray-900">Continue with Github</p>
+            <p className="ml-4 text-sm font-medium text-gray-900">
+              Continue with Github
+            </p>
           </button>
         </div>
       </div>
